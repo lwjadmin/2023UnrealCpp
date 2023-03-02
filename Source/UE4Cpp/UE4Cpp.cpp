7edac60776062a5df56069a369b2573286c739c8
wjlee <dndwpsla123@naver.com>
2023-03-02 11:37:07 +0900

    2023.03.02 이웅재 - C++ 프로젝트 추가 2022.08 ~ 2023.02간 학습한 Unreal4.27 Blueprint & C++ 소스코드를 이곳에서 합치며 작업할 예정!

---
+++ Source/UE4Cpp/UE4Cpp.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE4Cpp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4Cpp, "UE4Cpp" );
---
+++ Source/UE4Cpp/UE4CppGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4CppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4CPP_API AUE4CppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
